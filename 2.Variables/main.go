7099b01f1a1a21a2b18797815dfe2a77ba9b031a
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-07-18 10:26:12 +0530

    variables

---
+++ 2.Variables/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Variables")
+	// var username string = "ayush"
+	// fmt.Println(username)
+	// fmt.Printf("Variable is the type of: %T", username)
+
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Type of the Variable: %T", isLoggedIn)
+}
