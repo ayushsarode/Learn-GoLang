5a35caf17c392316927dbc9f0bc8f46eff6da602
ayushsarode <ayushsarode777@gmail.com>
2024-12-18 18:43:45 +0530

    added web

---
+++ 09.todo-List/main.go
@@ -0,0 +1,1 @@
+package _9_todo_List
--- 2.Variables/main.go
+++ 2.Variables/main.go
@@ -1,19 +1,17 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
-//declaring const 
-	const Value string = "efn3qogeovnv" //public
-
-
-
+// declaring const
+const Value string = "efn3qogeovnv" //public
 
 func main() {
 	fmt.Println("Variables")
 	var username string = "ayush"
 	fmt.Println(username)
 	fmt.Printf("Variable is the type of: %T", username)
-
 
 	var isLoggedIn bool = true
 	fmt.Println(isLoggedIn)
@@ -23,35 +21,29 @@ func main() {
 	fmt.Println(smallVal)
 	fmt.Printf("Variable is of type: %T \n", smallVal)
 
-
-
 	//float32 gives 5 digits after point
 	// var smallFloat float32 = 255.236374735737
 	var smallFloat float64 = 255.266264634646
 	fmt.Println(smallFloat)
 	fmt.Printf("Type of variable: %T \n", smallFloat)
 
-
-
 	//default values and some aliases
 	var anotherVariable int
 	fmt.Println(anotherVariable)
 	fmt.Printf("Type of variable: %T ", anotherVariable)
 
-
-	//in this its nothin blank 
+	//in this its nothin blank
 	var defaultStringVariable string
 	fmt.Println(defaultStringVariable)
 	fmt.Printf("Type %T \n", defaultStringVariable)
 
-
-	// implicit type 
+	// implicit type
 	var website = "www.google.com"
 	fmt.Println(website)
 
-	// no var style 
-	
-	numberOfUser:= 30000.0
+	// no var style
+
+	numberOfUser := 30000.0
 	fmt.Println(numberOfUser)
 
 	// there is a way to opt-out of declaring the type of a variable by using the walrus operator
@@ -62,9 +54,5 @@ func main() {
 
 	fmt.Println(Value)
 
-
-
-
 	maths()
 }
-
